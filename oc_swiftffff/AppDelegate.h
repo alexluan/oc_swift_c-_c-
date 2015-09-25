9b6ffb5958891973fcc4c15134e9bd4ba5936265
alexluan <326083325@qq.com>
2015-09-25 19:38:16 +0800

    Initial Commit

---
+++ oc_swiftffff/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  oc_swiftffff
+//
+//  Created by 栾有数 on 15/9/25.
+//  Copyright (c) 2015年 栾有数. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ oc_swiftffff/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  oc_swiftffff
+//
+//  Created by 栾有数 on 15/9/25.
+//  Copyright (c) 2015年 栾有数. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
